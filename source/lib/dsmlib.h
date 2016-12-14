9e0511e5e3488bb3931927ada31a38ff56a26098
dwarakarashmi <dwarakarashmi@ccs.neu.edu>
2016-12-14 05:06:57 -0500

    header file updated

--- source/lib/dsmlib.h
+++ source/lib/dsmlib.h
@@ -2,15 +2,18 @@
 #define _DSM_LIB_H
 
 #include <err.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <pthread.h>
-#include <stdint.h>
+#include <string.h>
+#include <sys/mman.h>
 #include <ucontext.h>
  
 // Constants
-#define MAX_SHARED_AREAS 1000000
+#define MAX_SHARED_AREAS 100
 
 // Data definitions
 struct shared_area {
@@ -19,10 +22,12 @@ struct shared_area {
 };
 
 // Functions
-int dsmlib_init(char *ip, int port, uintptr_t starta, size_t len);
+int dsmlib_init(char *ip, int port, uintptr_t start, size_t len);
 int dsmlib_destroy(void);
 void page_fault_handler(int signum, siginfo_t *siginfo, ucontext_t *cont);
-int write_handler(void *pg);
-int read_handler(void *pg);
+int write_handler(int page_number);
+int read_handler(int page_number);
+int read_write_handler(int page_number,char* operation);
+int add_shared_area(uintptr_t start, size_t len);
 
 #endif
